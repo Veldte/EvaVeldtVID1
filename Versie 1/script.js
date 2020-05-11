49bc326051e2bb2965de3989a06c1cc00583d10c
Veldte <47381095+Veldte@users.noreply.github.com>
2020-05-11 12:44:15 +0200

    Add files via upload

---
+++ Versie 1/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var element = document.getElementById("body");
+  element.classList.toggle("invert");
+}
+
+function myFunctiontwo() {
+  var element = document.getElementById("body");
+  element.classList.toggle("turn");
+}
+
