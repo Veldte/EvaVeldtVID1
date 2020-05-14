c224aa6f0a81f4ad38e7ff1886e89e78ab767bdc
Veldte <47381095+Veldte@users.noreply.github.com>
2020-05-14 15:01:54 +0200

    Add files via upload

---
+++ Versie 2/script.js
@@ -0,0 +1,68 @@
+function myFunction() {
+  var element = document.getElementById("body");
+  element.classList.toggle("invert");
+}
+
+function myFunctiontwo() {
+  var element = document.getElementById("body");
+  element.classList.toggle("turn");
+}
+
+
+
+
+function myFunctionthree() {
+  var element = document.getElementById("body");
+  element.classList.toggle("geelgroen");
+}
+
+function myFunctionfour() {
+  var element = document.getElementById("body");
+  element.classList.toggle("blauwrood");
+}
+
+function myFunctionfive() {
+  var element = document.getElementById("body");
+  element.classList.toggle("oranjeroze");
+}
+
+function myFunctionsix() {
+  var element = document.getElementById("body");
+  element.classList.toggle("paarsgeel");
+}
+
+function myFunctionseven() {
+  var element = document.getElementById("body");
+  element.classList.toggle("groenblauw");
+}
+
+function myFunctioneight() {
+  var element = document.getElementById("body");
+  element.classList.toggle("zwartroze");
+}
+
+function myFunctionnine() {
+  var element = document.getElementById("body");
+  element.classList.toggle("oranjegeel");
+}
+
+function myFunctionten() {
+  var element = document.getElementById("body");
+  element.classList.toggle("blauwgeel");
+}
+
+function myFunctioneleven() {
+  var element = document.getElementById("body");
+  element.classList.toggle("zwartblauw");
+}
+
+function myFunctiontwelve() {
+  var element = document.getElementById("body");
+  element.classList.toggle("roodgroen");
+}
+
+function myFunctionthirteen() {
+  var element = document.getElementById("body");
+  element.classList.toggle("oranjeblauw");
+}
+
